057205545ad7de6d03ef728ba6aca9624cc376f0
BBBB0601 <wlstmdgh1@gmail.com>
2026-07-24 11:13:39 +0900

    플레이어 섹터 좌표를 주기적으로 체크하는 로직 추가

--- Assets/Script/JinSeungHo/Sector/SectorManager.cs
+++ Assets/Script/JinSeungHo/Sector/SectorManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class SectorManager : MonoBehaviour
@@ -11,6 +12,11 @@ public class SectorManager : MonoBehaviour
     [Header("현재 플레이어가 위치한 섹터 좌표"), SerializeField]
     private Vector2Int _currPlayerPos;
     public Vector2Int CurrPlayerPos => _currPlayerPos;
+
+    [Header("플레이어 좌표 체크 주기"), SerializeField]
+    private float _checkingInterval;
+
+    [Header("플레이어")]    private GameObject _player;
 
     /// <summary>
     /// 현재 플레이어가 위치한 섹터의 삭제 프로토콜
@@ -30,6 +36,7 @@ public class SectorManager : MonoBehaviour
         if(Instance == null)    Instance = this;
         else                    Destroy(gameObject);
 
+        if(_player == null) _player = GameObject.FindGameObjectWithTag("Player");
         _currPlayerPos = new Vector2Int(0, 0);
 
         if(_sectorSpawner == null)  _sectorSpawner = GetComponent<SectorSpawn>();
@@ -40,6 +47,11 @@ public class SectorManager : MonoBehaviour
         _currSectorDeleteProtocol = _sectorMap[_currPlayerPos].GetComponent<DeleteProtocol>();
 
         _currStayDuration = 0;
+        
+        // 좌표 확인 주기는 0.1f초 마다 수행
+        if(_checkingInterval < 0.1f)  _checkingInterval = 0.1f;
+
+        StartCoroutine(Co_CheckPlayerSectorPosition());
     }
 
     private void Update()
@@ -52,17 +64,24 @@ public class SectorManager : MonoBehaviour
         // 플레이어에게 입힐 퍼센트 데미지 비례 양을 계산 후 플레이어 HP에 반영
     }
 
-    /// <summary>
-    /// 플레이어 섹터 위치를 인자 currPos의 위치로 업데이트
-    /// </summary>
-    /// <param name="currPos">현재 위치</param>
-    private void UpdateCurrPlayerPosition(Vector2Int currPos)
+    private IEnumerator Co_CheckPlayerSectorPosition()
     {
-        _currPlayerPos = currPos;
+        WaitForSeconds delay = new WaitForSeconds(_checkingInterval);
+        while(true)
+        {
+            if(_player == null) break;
 
-        _currSectorDeleteProtocol = _sectorMap[currPos].GetComponent<DeleteProtocol>();
+            int px = Mathf.RoundToInt(_player.transform.position.x / SectorData.WIDTH);
+            int py = Mathf.RoundToInt(_player.transform.position.y / SectorData.HEIGHT);
 
-        _currStayDuration = _currSectorDeleteProtocol.StayDuration;
+            _currPlayerPos = new Vector2Int(px, py);
+
+            _currSectorDeleteProtocol = _sectorMap[_currPlayerPos].GetComponent<DeleteProtocol>();
+
+            _currStayDuration = _currSectorDeleteProtocol.StayDuration;
+
+            yield return delay;
+        }
     }
 
     [ContextMenu("테스트/섹터 생성")]
