091147bca66d1e1c3e1893877f8068c58f117c49
BBBB0601 <wlstmdgh1@gmail.com>
2026-07-28 14:14:03 +0900

    침투 모듈 시스템 정의
    - 침투 모듈 시스템의 모듈 데이터를 Scriptable Object로 작성 완료
    - 모듈 7종을 정의하는 ModuleEffectType 작성 완료

---
+++ Assets/Script/JinSeungHo/InfiltrationModuleData/ModuleEffectType.cs
@@ -0,0 +1,12 @@
+public enum ModuleEffectType
+{
+    None = 0,
+
+    AttackPower,        // 증폭
+    AttackSpeed,        // 광속
+    DashCooldown,       // 안정
+    CriticalChance,     // 치명
+    ArmorPenetration,   // 돌파
+    LifeSteal,          // 흡수
+    Berserk             // 폭주
+}
