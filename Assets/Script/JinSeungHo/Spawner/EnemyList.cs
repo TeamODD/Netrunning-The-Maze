de9434fd98125d02d378d7c8af5733f8d80b9bc7
BBBB0601 <wlstmdgh1@gmail.com>
2026-08-07 13:11:32 +0900

    적 스포너 로직 작성 완료
    - 각 섹터의 스폰 포인트 리스트와 적 생성량을 SpawnEnemy에 전달시 각 섹터의 포인트 리스트 중 랜덤하게 적을 채워나감

--- Assets/Script/JinSeungHo/Spawner/EnemyList.cs
+++ Assets/Script/JinSeungHo/Spawner/EnemyList.cs
@@ -4,7 +4,9 @@ public class EnemyList : MonoBehaviour
 {
     [Header("근접 적 프리팹 리스트"), SerializeField]
     private GameObject[] _meleeEnemy;
+    public GameObject[] MeleeEnemy;
 
     [Header("드론 적 프리팹 리스트"), SerializeField]
     private GameObject[] _droneEnemy;
+    public GameObject[] DroneEnemy;
 }
--- Assets/Script/JinSeungHo/Spawner/EnemySpawner.cs
+++ Assets/Script/JinSeungHo/Spawner/EnemySpawner.cs
@@ -1,16 +1,44 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static EnemySpawner Instance;
+
+    [SerializeField]
+    private EnemyList _enemyList;
+
     void Start()
     {
-        
+        if(Instance == null)    Instance = this;
+        else                    Destroy(gameObject);
+
+        if(_enemyList == null)   _enemyList = GetComponent<EnemyList>();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 적 스폰 포인트 리스트 중 스폰량만큼 적을 생성함
+    /// </summary>
+    /// <param name="spawnPointList">적 스폰 포인트 리스트</param>
+    /// <param name="amount">적 스폰량</param>
+    public void SpawnEnemy(Transform[] spawnPointList, int amount)
     {
-        
+        // [0, 적 스폰 포인트 리스트 수] 만큼만 생성하게 함
+        amount = Mathf.Clamp(amount, 0, spawnPointList.Length);
+
+        List<Transform> tmpPoints = new List<Transform>(spawnPointList);
+        int tmpLength = tmpPoints.Count;
+
+        for(int i = 0; i < amount; ++i, --tmpLength)
+        {
+            int randIndex = Random.Range(0, tmpLength);
+
+            // test : 우선 근접 적만 출현하게 함
+            GameObject enemy = _enemyList.MeleeEnemy[0];
+            Instantiate(enemy, tmpPoints[randIndex].position, Quaternion.identity);
+
+            Transform last = tmpPoints[tmpLength - 1];
+            tmpPoints[randIndex] = last;
+        }
     }
 }
